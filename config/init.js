8b61d8cb9fec65bcf04c87b6ce21c7fb67f87693
Luque Hector <luque.hectorluis@gmail.com>
2020-11-17 19:48:48 -0300

    segundo commit

--- config/init.js
+++ config/init.js
@@ -1,10 +1,13 @@
 let config = {
     port:3002,
     database:{
-        host: '52.14.22.254',
+        host: 'localhost',
         user: 'root',
         password:'USrrh7H6fb',
-        database:'trazabilidad'
+        database:'trazabilidad'//,
+//          host: 'localhost',
+//          user: 'root',
+//          password:'pepirimini'
     },
     /*database:{
         host: 'localhost',
--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,31 @@
 const pool = require('../config/database');
-
+const qrcode = require('qrcode');
 
 exports.addPersona= async (req, res) =>{
     let persona = req.body;
-    await pool.query('INSERT INTO persona set ?', [persona], function(err, sql, fields){
+    let cadena = persona.dni+persona.num_tramite+persona.Nombre+persona.Apellido+persona.email;
+
+
+    const qr = await qrcode.toDataURL(cadena);
+
+    let   persona2 = {
+          dni: persona.dni,
+          Apellido: persona.Apellido,
+          Nombre : persona.Nombre,
+          telefono: persona.telefono,
+          num_tramite: persona.num_tramite,
+          email:persona.email,
+          positivo: 0,
+          qr:qr
+      }
+
+    console.log(persona2);
+
+
+    await pool.query('INSERT INTO persona set ?', [persona2], function(err, sql, fields){
            if(err){
                res.status(400).json({
-                   error: 'No se ha podido guardar el persona'+err
+                   error: 'No se ha podido guardar el persona '+err
                });
            }
            else{
@@ -14,6 +33,17 @@ exports.addPersona= async (req, res) =>{
                res.status(200).send({sql});
            }
     });
+
+    /*console.log(dataQR);
+    let qr = await qrcode.toDataURL(dataQR)
+      .then(dataQR => {
+        console.log(dataQR)
+      })
+        .catch(err => {
+          console.error(err)
+        });
+    console.log(qr);*/
+
 }
 
 
