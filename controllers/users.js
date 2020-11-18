735a7837cf713c9c6852e23322b98c4cd1658d65
Luque Hector <luque.hectorluis@gmail.com>
2020-11-18 11:29:03 -0300

    terminado la API addComercio, con envio de datos usuarios/clave por mail

--- controllers/users.js
+++ controllers/users.js
@@ -5,21 +5,21 @@ const PDF = require('html-pdf');
 const fs = require('fs');
 const Path = require('path');
 const PDFDocument = require('pdfkit');
-
+const Str = require('@supercharge/strings')
 
 
 
 exports.addPersona= async (req, res) =>{
     let persona = req.body;
-    let cadena = persona.dni+"-"+persona.num_tramite+"-"+persona.Nombre+"-"+persona.Apellido+"-"+persona.email;
+    let cadena = persona.dni+"-"+persona.num_tramite+"-"+persona.nombre+"-"+persona.apellido+"-"+persona.email;
 
 
     const qr = await qrcode.toDataURL(cadena);
 
     let   persona2 = {
           dni: persona.dni,
-          Apellido: persona.Apellido,
-          Nombre : persona.Nombre,
+          apellido: persona.apellido,
+          nombre : persona.nombre,
           telefono: persona.telefono,
           num_tramite: persona.num_tramite,
           email:persona.email,
@@ -44,20 +44,20 @@ exports.addPersona= async (req, res) =>{
               var imgData = qr;
               var base64Data = imgData.replace(/^data:image\/png;base64,/, "");
 
-              fs.writeFile("./public/pdf/qr-"+persona2.Apellido+"-"+persona2.Nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".png", base64Data, 'base64',
+              fs.writeFile("./public/pdf/qr-"+persona2.apellido+"-"+persona2.nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".png", base64Data, 'base64',
                   function(err, data) {
                   if (err) {
                          console.log('err', err);
                   }
                   console.log('success');
                   const doc = new PDFDocument;
-                  doc.pipe(fs.createWriteStream("./public/pdf/pdf-"+persona2.Apellido+"-"+persona2.Nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".pdf"));
+                  doc.pipe(fs.createWriteStream("./public/pdf/pdf-"+persona2.apellido+"-"+persona2.nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".pdf"));
                   const content = `
-                  Código QR para la persona  `+persona2.Apellido+ ` -   `+persona2.Nombre+ `
+                  Código QR para la persona  `+persona2.apellido+ ` - `+persona2.nombre+ `
                   Gobierno de la Provincia de Corrientes - 2020
                   Mail: `+persona2.email;
                   doc.text(content, 100, 100);
-                  doc.image("./public/pdf/qr-"+persona2.Apellido+"-"+persona2.Nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".png", 320, 280, {scale: 1})
+                  doc.image("./public/pdf/qr-"+persona2.apellido+"-"+persona2.nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".png", 320, 280, {scale: 1})
                       .text('Código QR', 320, 265);
                   doc.end();
 
@@ -84,7 +84,7 @@ exports.addPersona= async (req, res) =>{
                  attachments:[
                     {
                     //"path": "./public/images/logo.png"
-                    "path": "./public/pdf/pdf-"+persona2.Apellido+"-"+persona2.Nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".pdf"
+                    "path": "./public/pdf/pdf-"+persona2.apellido+"-"+persona2.nombre+"-"+persona2.dni+"-"+persona2.num_tramite+".pdf"
                     }
                     ]
                };
@@ -100,31 +100,57 @@ exports.addPersona= async (req, res) =>{
 
 
 
-    /*console.log(dataQR);
-    let qr = await qrcode.toDataURL(dataQR)
-      .then(dataQR => {
-        console.log(dataQR)
-      })
-        .catch(err => {
-          console.error(err)
-        });
-    console.log(qr);*/
-
 }
 
 
 
 exports.addComercio= async (req, res) =>{
     let comercio = req.body;
-    await pool.query('INSERT INTO comercio set ?', [comercio], function(err, sql, fields){
+    const random = Str.random(10)
+    let   comercio2 = {
+          cuit: comercio.cuit,
+          apellido: comercio.apellido,
+          nombre : comercio.nombre,
+          telefono: comercio.telefono,
+          direccion: comercio.direccion,
+          email:comercio.email,
+          usuario: comercio.email,
+          password: random
+      }
+
+
+    await pool.query('INSERT INTO comercio set ?', [comercio2], function(err, sql, fields){
            if(err){
                res.status(400).json({
-                   error: 'No se ha podido guardar el comercio'
+                   error: 'No se ha podido guardar el comercio'+err
                });
            }
            else{
                console.log('Se guardo correctamente');
                res.status(200).send({sql});
+               //envio de mail
+               var transporter = nodemailer.createTransport({
+                 service: 'Gmail',
+                 auth: {
+                   user: 'flf.solucionesysistemas@gmail.com',
+                   pass: 'everLAST2020'
+                 }
+               });
+
+               var mailOptions = {
+                 from: 'Trazate',
+                 to: comercio2.email,
+                 subject: 'Mail de registracion',
+                 text: 'Mail donde se envian usuario y clave para el acceso a la plataforma',
+                 html: '<h1>Bienvenido al primer sistema de trazabilidad en tiempo real, Su QR listo para ser usado</h1> <p>usuario: '+comercio2.email+' - clave: '+random+' </p>'
+               };
+               transporter.sendMail(mailOptions, function(error, info){
+                 if (error) {
+                   console.log(error);
+                 } else {
+                   console.log('Email sent: ' + info.response);
+                 }
+               });
            }
     });
 
