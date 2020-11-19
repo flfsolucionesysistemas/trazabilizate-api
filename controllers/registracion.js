4d549f73170d945835c9d7c5975bb1ec90d48bcd
Luque Hector <luque.hectorluis@gmail.com>
2020-11-18 21:03:21 -0300

    func de checkIn

---
+++ controllers/registracion.js
@@ -0,0 +1,36 @@
+const pool = require('../config/database');
+
+
+//begin addPersona
+exports.addCheckIn=  async (req, res) =>{
+
+  let params = req.body;
+  let comercio = params.id_comercio;
+  let qrcode = params.qrcode;
+  let fechaHora= new Date();
+
+  //consulta a la base si el qrcode existe en persona, y me trae los datos de la persona
+  let row = await pool.query('select * from persona where qrcode = ?', [qrcode]);
+  if (row.length>0){
+    console.log(row[0]);
+      let registracion = {
+        id_comercio : comercio,
+        fecha_hora : fechaHora,
+        id_persona : row[0].id_Persona
+      }
+     await pool.query('INSERT INTO registracion set ?', [registracion], function(err, sql, fields){
+        if(err){
+            res.status(400).json({
+                error: 'No se ha podido guardar el comercio'+err
+            });
+        }
+        else{
+          console.log('Se guardo correctamente');
+          res.status(200).send({sql});
+        }
+      });
+  }else{
+    console.log('no existe qrcode en la base');
+  }
+
+}
